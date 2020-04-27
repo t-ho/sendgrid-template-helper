190be9dbcf65d34adf979d4bfb3fd3a7cb61a16e
t-ho <toan.hmt@protonmail.com>
2020-04-26 21:30:06 -0700

    Add prettier config

--- example.js
+++ example.js
@@ -1,19 +1,19 @@
-const path = require("path");
-const Email = require("./lib");
+const path = require('path');
+const Email = require('./lib');
 
 const email = new Email({
   apiKey: process.env.SENDGRID_API_KEY,
-  prefix: "your_app_name_",
+  prefix: 'your_app_name_',
 });
 
 email
   .send({
-    to: "user@tdev.app",
-    from: "admin@tdev.app",
+    to: 'user@tdev.app',
+    from: 'admin@tdev.app',
     subject: `[Test] Sendgrid template helper`,
-    templatePath: path.resolve(__dirname, "./dynamic-email-template.html"),
+    templatePath: path.resolve(__dirname, './dynamic-email-template.html'),
     dynamicTemplateData: {
-      username: "user",
+      username: 'user',
     },
   })
   .then(() => {})
